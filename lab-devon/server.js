419d82d0b51588923b19cfebebdea7a67e6625f1
devonhackley <devonhackley@gmail.com>
2017-01-10 13:58:16 -0800

    starter files

---
+++ lab-devon/server.js
@@ -0,0 +1,45 @@
+'use strict';
+const net = require('net');
+const server = net.createServer();
+const parse = require('./lib/parse-message.js');
+
+let sockets = [];
+
+server.on('connection', function(socket){
+  console.log('A Client Connected!');
+  socket.write('Hey! Welcome to Wack Chat');
+  socket.username = `user_${Math.floor(Math.random()*100)}`;
+  sockets.push(socket);
+
+  socket.on('data', function(buffer){
+    let message = buffer.toString();
+
+    if(message.startsWith('/nick'))
+      return parseMessage.nickCommand(message,socket);
+
+    if(message.startsWith('/dm'))
+      return parseMessage.dmCommand(message,sockets);
+
+    if(message.startsWith('/users'))
+      return parseMessage.users(sockets, socket);
+
+    if(message.startsWith('/troll'))
+      return parseMessage.trollCommand(message,sockets);
+
+    if(message.startsWith('/ban'))
+      return parseMessage.banCommand(message, sockets);
+
+  });
+
+  socket.on('close', function(){
+    console.log('a client left the chatroom');
+    sockets.forEach(function(s, index){
+      if(s === socket)
+        sockets.splice(index,1);
+    });
+  });
+});
+
+server.listen(3000, function(){
+  console.log('Server is up!');
+});
